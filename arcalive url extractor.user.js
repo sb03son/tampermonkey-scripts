602442311617bc69fa849f4184f36dbfb6af70af
Hanyang <hyson22@outlook.kr>
2025-11-23 15:28:42 +0900

    Update arcalive url extractor.user.js

--- arcalive url extractor.user.js
+++ arcalive url extractor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         아카라이브 게시글 URL 추출
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  아카라이브에서 게시글 URL 추출 + 읽음무시 + 이미지글 필터링 + 무제한 페이지 지원
 // @author       kts + mod
 // @match        https://arca.live/b/*
